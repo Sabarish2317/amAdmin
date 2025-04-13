12df770f88f70fe4f81558622ea07d0013a75a14
Sabarish V S <Sabarish_7@protonmail.com>
2025-04-13 12:23:34 +0530

    checks

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 
 const dotenv = require("dotenv");
 dotenv.config();
-
+//config
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
